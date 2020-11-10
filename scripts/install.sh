3d763912387f747dd3063e494433aab3a446db89
Felipe Pattillo <fpattillo@uc.cl>
2020-11-10 08:44:53 -0300

    change travis & install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,5 +2,5 @@
 pwd=$( aws ecr get-login-password )
 docker container stop $(docker container ls -aq)
 docker login -u AWS -p $pwd https://780722053658.dkr.ecr.us-east-2.amazonaws.com
-docker pull 780722053658.dkr.ecr.us-east-2.amazonaws.com/Arquichat
+docker pull 780722053658.dkr.ecr.us-east-2.amazonaws.com/arquichat-repo
 
