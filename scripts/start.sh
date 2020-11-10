3a4d63b1cc91ed06271c98cbf4681d7fb6094655
Felipe Pattillo <fpattillo@uc.cl>
2020-11-10 10:49:02 -0300

    change scripts & yml

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker-compose -f /home/ubuntu/fp_personal/docker-compose.production.yml up -d
+docker-compose up -d
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker-compose -f /home/ubuntu/fp_personal/docker-compose.production.yml down
+docker-compose down
 docker stop $(docker ps -a -q)
