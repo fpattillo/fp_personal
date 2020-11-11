68269a10bed66a1e93d8c225020f5efabcc5820d
Felipe Pattillo <fpattillo@uc.cl>
2020-11-11 01:01:22 -0300

    add region to script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pwd=$( aws ecr get-login-password )
+pwd=$( aws ecr get-login-password --region us-east-2 )
 docker container stop $(docker container ls -aq)
 docker login -u AWS -p $pwd https://780722053658.dkr.ecr.us-east-2.amazonaws.com
 docker pull 780722053658.dkr.ecr.us-east-2.amazonaws.com/arquichat-repo
