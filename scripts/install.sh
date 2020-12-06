b173d379b465956c9617c85d84240461d783671c
Felipe Pattillo <fpattillo@uc.cl>
2020-12-06 12:27:08 -0300

    revert stop script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pwd=$( aws ecr get-login-password --region us-east-2 )
 docker container stop $(docker container ls -aq)
-docker login -u AWS -p $pwd https://780722053658.dkr.ecr.us-east-2.amazonaws.com
+docker login -u AWS -p $pwd https://780722053658.dkr.ecr.us-east-2.amazonaws.com/arquichat-repo
 docker pull 780722053658.dkr.ecr.us-east-2.amazonaws.com/arquichat-repo
 
