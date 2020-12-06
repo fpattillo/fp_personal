c0ddaedfd54b21711baeb8a75b912efeb4cb4122
Felipe Pattillo <fpattillo@uc.cl>
2020-12-06 03:54:42 -0300

    eliminate docker stop

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 docker-compose -f /home/ubuntu/iic2173-proyecto-semestral-grupo9/docker-compose.yml down
-docker stop $(docker ps -a -q | xargs -r sudo docker stop)
-docker stop $(docker ps -a -q | xargs -r sudo docker rm)
+
