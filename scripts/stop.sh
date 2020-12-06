a4469f0ec98f302a00f074137494aa05dec3f5e7
Felipe Pattillo <fpattillo@uc.cl>
2020-12-06 03:27:22 -0300

    add no container error fix

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker stop $(docker ps -a -q)
 docker-compose -f /home/ubuntu/iic2173-proyecto-semestral-grupo9/docker-compose.yml down
-
+docker stop $(docker ps -a -q | xargs -r sudo docker stop)
+docker stop $(docker ps -a -q | xargs -r sudo docker rm)
