1039d37178f3c4b3308c17a80713f4ec34d42932
Felipe Pattillo <fpattillo@uc.cl>
2020-12-06 03:05:29 -0300

    change script order

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-docker-compose -f /home/ubuntu/iic2173-proyecto-semestral-grupo9/docker-compose.yml down
 docker stop $(docker ps -a -q)
+docker-compose -f /home/ubuntu/iic2173-proyecto-semestral-grupo9/docker-compose.yml down
+
